4d9efe1bdf4b1ca0c07efc0144fcae390493a621
RiscHanma <141110636+RiscHanma@users.noreply.github.com>
2023-10-19 14:22:16 +0700

    Add files via upload

---
+++ Tugas Mandiri-4/Program bilangan bulat positif.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int bilangan;
+
+    printf("Masukkan sebuah bilangan bulat positif: ");
+    scanf("%d", &bilangan);
+
+    if (bilangan < 0) {
+        printf("Bilangan yang dimasukkan bukan bilangan bulat positif.\n");
+    } else if (bilangan % 2 == 0) {
+        printf("GENAP\n");
+    } else {
+        printf("GANJIL\n");
+    }
+
+    return 0;
+}
